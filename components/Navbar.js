b4827c9e4a419141dad9d1d7b45dbb0ea95e6652
EgorDyakonov <egor.dyakonov666@gmail.com>
2021-04-26 19:23:23 +0300

    second commit

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { Profile } from './Profile';
-// import MainContent from './MainContent';
 import MainContainer from './MainContainer';
 
 export const Navbar = () => {
